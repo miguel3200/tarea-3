b9b61db9d98941c5265ea00dcc710946b2ca176e
miguel3200 <55986443+miguel3200@users.noreply.github.com>
2020-04-09 15:10:12 -0500

    Add files via upload

---
+++ tipos de variables.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+
+int main() {
+
+	std::cout << "size of char: " << sizeof(char) << " byte" << "\n";
+	std::cout << "size of unsigned char: " << sizeof(unsigned char) << " byte" << "\n";
+	std::cout << "size of int: " << sizeof(int) << " bytes" << "\n";
+	std::cout << "size of unsigned int: " << sizeof(unsigned int) << " bytes" << "\n";
+	std::cout << "size of unsigned short int: " << sizeof(unsigned short int) << " bytes" << "\n";
+	std::cout << "size of short int: " << sizeof(short int) << " bytes" << "\n";
+	std::cout << "size of long int: " << sizeof(long int) << " bytes" << "\n";
+	std::cout << "size of signed long int: " << sizeof(signed long int) << " bytes" << "\n";
+	std::cout << "size of unsigned long int: " << sizeof(unsigned long int) << " bytes" << "\n";
+	std::cout << "size of long long int: " << sizeof(long long int) << " bytes" << "\n";
+	std::cout << "size of unsigned long long int: " << sizeof(unsigned long long int) << " bytes" << "\n";
+	std::cout << "size of float: " << sizeof(float) << " bytes" << "\n";
+	std::cout << "size of double: " << sizeof(double) << " bytes" << "\n";
+	std::cout << "size of long double: " << sizeof(long double) << " bytes" << "\n";
+
+}
